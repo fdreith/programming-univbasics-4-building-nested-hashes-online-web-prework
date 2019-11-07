5e754ed86eda25faa615373b1e382e7863fc91a4
Francesca Dreith <francesca.dreith@gmail.com>
2019-11-07 20:53:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	     :pieces=> 4,
 	     :rent_in_dollars=> 
 	     {
-	       :one=>"",
+	       :one_piece_owned=>25,
 	       :two=>"",
 	       :three=>"",
 	       :four=>""
